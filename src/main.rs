665cda0b99dfee8f567901f2430c434e034e8738
smaldonado411 <smaldonado@distelsa.com.gt>
2025-10-21 00:51:45 -0600

    Scanning: Parentheses

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,14 @@ fn main() {
             });
 
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
+                for c in file_contents.chars() {
+                    match c {
+                        '(' => println!("LEFT_PAREN ( null"),
+                        ')' => println!("RIGHT_PAREN ) null"),
+                        _ => (),
+                    }
+                }
+                println!("EOF  null");
             } else {
                 println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
             }
