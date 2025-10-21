e8dd6abe2217930c06516484f21a5b7002c332c4
smaldonado411 <smaldonado@distelsa.com.gt>
2025-10-21 00:54:13 -0600

    Scanning: Braces

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,8 @@ fn main() {
                     match c {
                         '(' => println!("LEFT_PAREN ( null"),
                         ')' => println!("RIGHT_PAREN ) null"),
+                        '{' => println!("LEFT_BRACE {{ null"),
+                        '}' => println!("RIGHT_BRACE }} null"),
                         _ => (),
                     }
                 }
